7ab74a11536813edf032e53e18ff57698ef08af0
dahasaln <dahasaln@mail.ru>
2023-04-03 12:38:15 +0300

    03.04.2023 1- str 97 add valid_firstname valid_lastname

--- tests/test_positive_auth_page.py
+++ tests/test_positive_auth_page.py
@@ -1,35 +1,39 @@
 import pickle
 import time
 import pytest
+from selenium.webdriver.common.by import By
 from pages.auth import AuthPage
+from pages.locators import AuthLocators
 from pages.settings import valid_phone, valid_login, valid_password, \
-    invalid_ls, valid_email, valid_pass_reg, fake_email
+    invalid_ls, valid_email, valid_pass_reg, fake_email, valid_firstname, valid_lastname
+
+
+# @pytest.mark.auth
+# def test_active_tab(browser):
+#     """Проверка автоматического переключения табов тел/почта/логин/лицевой счет"""
+#
+#     browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_PHON)
+#     time.sleep(3)
+#     browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_MAIL).click()
+#     time.sleep(3)
+#     browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_LOGIN).click()
+#     time.sleep(3)
+#     browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_LS).click()
+#
+#     # assert browser.find_element(By.XPATH, '//*[@id="page-right"]/div/div/div/form/div[1]/div[2]/div/span[2]').text == 'Мобильный телефон'
+#     #
+#     # assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_MAIL).text == 'Почта'
+#     #
+#     # assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_LOGIN).text == 'Логин'
+#     #
+#     # assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB_LS).text == 'Лицевой счет'
+
+
+
 
 
 @pytest.mark.auth
 @pytest.mark.positive
-@pytest.mark.xfail
-@pytest.mark.parametrize('username', [valid_phone, valid_email, valid_login, invalid_ls],
-                         ids=['phone', 'email', 'login', 'ls'])
-def test_active_tab(browser, username):
-    """Проверка автоматического переключения табов тел/почта/логин/лицевой счет"""
-
-    page = AuthPage(browser)
-    page.enter_username(username)
-    page.enter_password(valid_password)
-    if username == valid_phone:
-        assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB).text == 'Телефон'
-    elif username == valid_email:
-        assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB).text == 'Почта'
-    elif username == valid_login:
-        assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB).text == 'Логин'
-    else:
-        assert browser.find_element(*AuthLocators.AUTH_ACTIVE_TAB).text == 'Лицевой счет'
-
-
-@pytest.mark.auth
-@pytest.mark.positive
-@pytest.mark.xfail
 @pytest.mark.parametrize('username', [valid_phone, valid_login],
                          ids=['valid phone', 'valid login'])
 def test_auth_page_phone_login_valid(browser, username):
@@ -52,7 +56,8 @@ def test_auth_page_email_valid(browser):
     time.sleep(25)     # на случай появления Captcha, необходимости ее ввода вручную
     page.btn_click_enter()
     page.driver.save_screenshot('auth_by_email.png')
-
+    print('Авторизация прошла успешно!')
+    print(f"{valid_firstname} {valid_lastname},\nВаш email: '{str(valid_email)}'\nВаш пароль: '{valid_pass_reg}'\n")
     with open('my_cookies.txt', 'wb') as cookies:
         pickle.dump(browser.get_cookies(), cookies)
 
